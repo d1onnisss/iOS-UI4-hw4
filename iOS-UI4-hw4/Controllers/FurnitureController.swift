45eb3641221645f8625517c1ad6f23b06bb3743f
d1onnisss <lim_al@auca.kg>
2024-05-20 20:46:50 +0600

    UITableViewDelegate implementation

---
+++ iOS-UI4-hw4/Controllers/FurnitureController.swift
@@ -0,0 +1,29 @@
+//
+//  FurnitureController.swift
+//  iOS-UI4-hw4
+//
+//  Created by Alexey Lim on 19/5/24.
+//
+
+import UIKit
+
+class FurnitureController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS-UI4-hw4/Model/Furniture.swift
@@ -0,0 +1,8 @@
+//
+//  Furniture.swift
+//  iOS-UI4-hw4
+//
+//  Created by Alexey Lim on 19/5/24.
+//
+
+import Foundation
